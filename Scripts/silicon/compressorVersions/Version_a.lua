cd907ba0bb82742c6f6ea702c644e09d821a9d18
niknal357 <46793548+niknal357@users.noreply.github.com>
2024-07-05 10:17:28 +0200

    fix table is nil bug

--- Scripts/silicon/compressorVersions/Version_a.lua
+++ Scripts/silicon/compressorVersions/Version_a.lua
@@ -1,78 +1,7 @@
 dofile "../../util/stringCompression/LibDeflate.lua"
+dofile "../../util/dataUtil.lua"
 
 SiliconCompressor.Versions["a"] = {}
-
-local numberToRotation = {
-    [1] = { sm.vec3.new(1, 0, 0), sm.vec3.new(0, 0, 1), sm.vec3.new(0, -1, 0) },
-    [2] = { sm.vec3.new(1, 0, 0), sm.vec3.new(0, -1, 0), sm.vec3.new(0, 0, -1) },
-    [3] = { sm.vec3.new(0, 1, 0), sm.vec3.new(0, 0, -1), sm.vec3.new(-1, 0, 0) },
-    [4] = { sm.vec3.new(0, 1, 0), sm.vec3.new(1, 0, 0), sm.vec3.new(0, 0, -1) },
-    [5] = { sm.vec3.new(0, 0, 1), sm.vec3.new(0, 1, 0), sm.vec3.new(-1, 0, 0) },
-    [6] = { sm.vec3.new(0, 0, 1), sm.vec3.new(-1, 0, 0), sm.vec3.new(0, -1, 0) },
-    [7] = { sm.vec3.new(0, -1, 0), sm.vec3.new(0, 0, -1), sm.vec3.new(1, 0, 0) },
-    [8] = { sm.vec3.new(0, 0, -1), sm.vec3.new(0, 1, 0), sm.vec3.new(1, 0, 0) },
-    [9] = { sm.vec3.new(0, 1, 0), sm.vec3.new(0, 0, 1), sm.vec3.new(1, 0, 0) },
-    [10] = { sm.vec3.new(0, 0, 1), sm.vec3.new(0, -1, 0), sm.vec3.new(1, 0, 0) },
-    [11] = { sm.vec3.new(-1, 0, 0), sm.vec3.new(0, 0, 1), sm.vec3.new(0, 1, 0) },
-    [12] = { sm.vec3.new(0, 0, -1), sm.vec3.new(-1, 0, 0), sm.vec3.new(0, 1, 0) },
-    [13] = { sm.vec3.new(1, 0, 0), sm.vec3.new(0, 0, -1), sm.vec3.new(0, 1, 0) },
-    [14] = { sm.vec3.new(0, 0, 1), sm.vec3.new(1, 0, 0), sm.vec3.new(0, 1, 0) },
-    [15] = { sm.vec3.new(-1, 0, 0), sm.vec3.new(0, -1, 0), sm.vec3.new(0, 0, 1) },
-    [16] = { sm.vec3.new(0, -1, 0), sm.vec3.new(1, 0, 0), sm.vec3.new(0, 0, 1) },
-    [17] = { sm.vec3.new(1, 0, 0), sm.vec3.new(0, 1, 0), sm.vec3.new(0, 0, 1) },
-    [18] = { sm.vec3.new(0, 1, 0), sm.vec3.new(-1, 0, 0), sm.vec3.new(0, 0, 1) },
-    [19] = { sm.vec3.new(0, -1, 0), sm.vec3.new(0, 0, 1), sm.vec3.new(-1, 0, 0) },
-    [20] = { sm.vec3.new(0, 0, -1), sm.vec3.new(0, -1, 0), sm.vec3.new(-1, 0, 0) },
-    [21] = { sm.vec3.new(-1, 0, 0), sm.vec3.new(0, 0, -1), sm.vec3.new(0, -1, 0) },
-    [22] = { sm.vec3.new(0, 0, -1), sm.vec3.new(1, 0, 0), sm.vec3.new(0, -1, 0) },
-    [23] = { sm.vec3.new(-1, 0, 0), sm.vec3.new(0, 1, 0), sm.vec3.new(0, 0, -1) },
-    [24] = { sm.vec3.new(0, -1, 0), sm.vec3.new(-1, 0, 0), sm.vec3.new(0, 0, -1) },
-}
-
-local rotationToNumber = {
-    ["1000-10"] = 1,
-    ["10000-1"] = 2,
-    ["010-100"] = 3,
-    ["01000-1"] = 4,
-    ["001-100"] = 5,
-    ["0010-10"] = 6,
-    ["0-10100"] = 7,
-    ["00-1100"] = 8,
-    ["010100"] = 9,
-    ["001100"] = 10,
-    ["-100010"] = 11,
-    ["00-1010"] = 12,
-    ["100010"] = 13,
-    ["001010"] = 14,
-    ["-100001"] = 15,
-    ["0-10001"] = 16,
-    ["100001"] = 17,
-    ["010001"] = 18,
-    ["0-10-100"] = 19,
-    ["00-1-100"] = 20,
-    ["-1000-10"] = 21,
-    ["00-10-10"] = 22,
-    ["-10000-1"] = 23,
-    ["0-1000-1"] = 24,
-}
-
-local typeToNumber = {
-    andBlocks = 0,
-    orBlocks = 1,
-    xorBlocks = 2,
-    nandBlocks = 3,
-    norBlocks = 4,
-    xnorBlocks = 5,
-}
-
-local numberToType = {
-    [0] = "andBlocks",
-    [1] = "orBlocks",
-    [2] = "xorBlocks",
-    [3] = "nandBlocks",
-    [4] = "norBlocks",
-    [5] = "xnorBlocks",
-}
 
 SiliconCompressor.Versions["a"].compressBlocks = function (siliconBlock)
     if siliconBlock.data.blocks == nil then return {} end
@@ -96,9 +25,9 @@ SiliconCompressor.Versions["a"].compressBlocks = function (siliconBlock)
                 end
             end
         end
-        local rotNumber = rotationToNumber[tostring(block.rot[1].x) .. tostring(block.rot[1].y) .. tostring(block.rot[1].z) .. tostring(block.rot[3].x) .. tostring(block.rot[3].y) .. tostring(block.rot[3].z)]
+        local rotNumber = DataUtil.rotationToNumber[tostring(block.rot[1].x) .. tostring(block.rot[1].y) .. tostring(block.rot[1].z) .. tostring(block.rot[3].x) .. tostring(block.rot[3].y) .. tostring(block.rot[3].z)]
         blocks[(block.pos.x - 0.5) + (block.pos.y - 0.5) * siliconBlock.size[1] + (block.pos.z - 0.5) * siliconBlock.size[2] * siliconBlock.size[1] + 1] = {
-            rotNumber * 6 + typeToNumber[block.type],
+            rotNumber * 6 + DataUtil.typeToNumber[block.type],
             block.uuid,
             inputs,
             block.outputs,
@@ -110,14 +39,14 @@ SiliconCompressor.Versions["a"].compressBlocks = function (siliconBlock)
     for color, index in pairs(colorHash) do
         colorIndexHash[index] = color
     end
-    local dataString = siliconBlock:tableToString({ blocks, colorIndexHash })
+    local dataString = DataUtil.tableToString({ blocks, colorIndexHash })
     return LibDeflate:EncodeForPrint(LibDeflate:CompressDeflate(dataString))
 end
 
 SiliconCompressor.Versions["a"].decompressBlockData = function(siliconBlock, rawData)
     local blockData = LibDeflate:DecompressDeflate(LibDeflate:DecodeForPrint(rawData))
     if blockData == nil then return {} end
-    blockData = siliconBlock:stringToTable(blockData)
+    blockData = DataUtil.stringToTable(blockData)
     if blockData == nil then return {} end
     local colorIndexHash = blockData[2]
     local onlyBlockData = blockData[1]
---
+++ Scripts/util/dataUtil.lua
@@ -0,0 +1,205 @@
+DataUtil = {} or DataUtil
+
+DataUtil.numberToRotation = {
+    [1] = { sm.vec3.new(1, 0, 0), sm.vec3.new(0, 0, 1), sm.vec3.new(0, -1, 0) },
+    [2] = { sm.vec3.new(1, 0, 0), sm.vec3.new(0, -1, 0), sm.vec3.new(0, 0, -1) },
+    [3] = { sm.vec3.new(0, 1, 0), sm.vec3.new(0, 0, -1), sm.vec3.new(-1, 0, 0) },
+    [4] = { sm.vec3.new(0, 1, 0), sm.vec3.new(1, 0, 0), sm.vec3.new(0, 0, -1) },
+    [5] = { sm.vec3.new(0, 0, 1), sm.vec3.new(0, 1, 0), sm.vec3.new(-1, 0, 0) },
+    [6] = { sm.vec3.new(0, 0, 1), sm.vec3.new(-1, 0, 0), sm.vec3.new(0, -1, 0) },
+    [7] = { sm.vec3.new(0, -1, 0), sm.vec3.new(0, 0, -1), sm.vec3.new(1, 0, 0) },
+    [8] = { sm.vec3.new(0, 0, -1), sm.vec3.new(0, 1, 0), sm.vec3.new(1, 0, 0) },
+    [9] = { sm.vec3.new(0, 1, 0), sm.vec3.new(0, 0, 1), sm.vec3.new(1, 0, 0) },
+    [10] = { sm.vec3.new(0, 0, 1), sm.vec3.new(0, -1, 0), sm.vec3.new(1, 0, 0) },
+    [11] = { sm.vec3.new(-1, 0, 0), sm.vec3.new(0, 0, 1), sm.vec3.new(0, 1, 0) },
+    [12] = { sm.vec3.new(0, 0, -1), sm.vec3.new(-1, 0, 0), sm.vec3.new(0, 1, 0) },
+    [13] = { sm.vec3.new(1, 0, 0), sm.vec3.new(0, 0, -1), sm.vec3.new(0, 1, 0) },
+    [14] = { sm.vec3.new(0, 0, 1), sm.vec3.new(1, 0, 0), sm.vec3.new(0, 1, 0) },
+    [15] = { sm.vec3.new(-1, 0, 0), sm.vec3.new(0, -1, 0), sm.vec3.new(0, 0, 1) },
+    [16] = { sm.vec3.new(0, -1, 0), sm.vec3.new(1, 0, 0), sm.vec3.new(0, 0, 1) },
+    [17] = { sm.vec3.new(1, 0, 0), sm.vec3.new(0, 1, 0), sm.vec3.new(0, 0, 1) },
+    [18] = { sm.vec3.new(0, 1, 0), sm.vec3.new(-1, 0, 0), sm.vec3.new(0, 0, 1) },
+    [19] = { sm.vec3.new(0, -1, 0), sm.vec3.new(0, 0, 1), sm.vec3.new(-1, 0, 0) },
+    [20] = { sm.vec3.new(0, 0, -1), sm.vec3.new(0, -1, 0), sm.vec3.new(-1, 0, 0) },
+    [21] = { sm.vec3.new(-1, 0, 0), sm.vec3.new(0, 0, -1), sm.vec3.new(0, -1, 0) },
+    [22] = { sm.vec3.new(0, 0, -1), sm.vec3.new(1, 0, 0), sm.vec3.new(0, -1, 0) },
+    [23] = { sm.vec3.new(-1, 0, 0), sm.vec3.new(0, 1, 0), sm.vec3.new(0, 0, -1) },
+    [24] = { sm.vec3.new(0, -1, 0), sm.vec3.new(-1, 0, 0), sm.vec3.new(0, 0, -1) },
+}
+
+DataUtil.rotationToNumber = {
+    ["1000-10"] = 1,
+    ["10000-1"] = 2,
+    ["010-100"] = 3,
+    ["01000-1"] = 4,
+    ["001-100"] = 5,
+    ["0010-10"] = 6,
+    ["0-10100"] = 7,
+    ["00-1100"] = 8,
+    ["010100"] = 9,
+    ["001100"] = 10,
+    ["-100010"] = 11,
+    ["00-1010"] = 12,
+    ["100010"] = 13,
+    ["001010"] = 14,
+    ["-100001"] = 15,
+    ["0-10001"] = 16,
+    ["100001"] = 17,
+    ["010001"] = 18,
+    ["0-10-100"] = 19,
+    ["00-1-100"] = 20,
+    ["-1000-10"] = 21,
+    ["00-10-10"] = 22,
+    ["-10000-1"] = 23,
+    ["0-1000-1"] = 24,
+}
+
+DataUtil.typeToNumber = {
+    andBlocks = 0,
+    orBlocks = 1,
+    xorBlocks = 2,
+    nandBlocks = 3,
+    norBlocks = 4,
+    xnorBlocks = 5,
+}
+
+DataUtil.numberToType = {
+    [0] = "andBlocks",
+    [1] = "orBlocks",
+    [2] = "xorBlocks",
+    [3] = "nandBlocks",
+    [4] = "norBlocks",
+    [5] = "xnorBlocks",
+}
+
+function DataUtil.tableToString(table)
+    local str = ""
+    local i = 1
+    for k, v in pairs(table) do
+        local needsKey = k == i
+        if type(v) == "table" then
+            v = "{" .. DataUtil.tableToString(v) .. "}"
+        elseif type(v) ~= "string" then
+            v = tostring(v)
+        else
+            if tonumber(v) ~= nil then
+                v = v .. "'"
+            end
+        end
+        if type(k) ~= "string" then
+            k = tostring(k)
+        else
+            if tonumber(k) ~= nil then
+                k = k .. "'"
+            end
+        end
+        if string.sub(str, -1, -1) ~= "}" and string.sub(str, -1, -1) ~= "'" and string.sub(v, 1, 1) ~= "{" and str ~= "" then
+            str = str .. "|"
+        end
+        if needsKey then
+            i = i + 1
+            str = str .. v
+        else
+            str = str .. k .. "]" .. v
+        end
+    end
+    -- if string.sub(str, -1, -1) == "}" then
+    --     str = string.sub(str, 1, -2)
+    -- end
+    return str--string.sub(str, 2, -1)
+end
+
+function DataUtil.stringToTable(str)
+    local tbl = {}
+    local chunk = ""
+    local gettingTableString = false
+    local key = 1
+    local isString = false
+    local countKey = true
+    local depth = 0
+    local stringIndex = 1
+    for c in str:gmatch "." do
+        ::again::
+        if gettingTableString then
+            if c == "{" then
+                depth = depth + 1
+                chunk = chunk .. c
+            elseif c == "}" then
+                if depth > 0 then
+                    depth = depth - 1
+                    chunk = chunk .. c
+                else
+                    chunk = DataUtil.stringToTable(chunk)
+                    gettingTableString = false
+                    c = "|"
+                    goto again
+                end
+            else
+                chunk = chunk .. c
+            end
+        else
+            local endChunk = false
+            if c == "|" then
+                endChunk = true
+            elseif c == "{" then
+                if chunk ~= "" then
+                    endChunk = true
+                end
+                gettingTableString = true
+            elseif c == "}" then
+                endChunk = true
+            elseif c == "]" then
+                if chunk == "" then
+                    key = key - 1
+                    chunk = tbl[key]
+                    tbl[key] = nil
+                    if tonumber(chunk) ~= nil then
+                        chunk = tonumber(chunk)
+                    end
+                end
+                key = chunk
+                chunk = ""
+                countKey = false
+            elseif c == "'" then
+                isString = true
+                endChunk = true
+            else
+                chunk = chunk .. c
+            end
+
+            if endChunk then
+                if tonumber(chunk) ~= nil and not isString then
+                    chunk = tonumber(chunk)
+                end
+                isString = false
+                if countKey ~= nil then
+                    if chunk ~= "" then
+                        tbl[key] = chunk
+                    end
+                    if c == "}" then
+                        tbl = { tbl }
+                    end
+                    if chunk ~= "" then
+                        if countKey == true then
+                            key = key + 1
+                        else
+                            countKey = nil
+                        end
+                    end
+                    chunk = ""
+                end
+            end
+        end
+        if stringIndex == #str then
+            if gettingTableString then
+                chunk = DataUtil.stringToTable(chunk)
+                gettingTableString = false
+            end
+            c = "|"
+            stringIndex = stringIndex + 1
+            goto again
+        end
+        stringIndex = stringIndex + 1
+    end
+    return tbl
+end

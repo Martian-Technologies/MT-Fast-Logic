8f9c2f53d68bb1fccdb8d0d5b24ad8aedd9fc711
niknal357 <46793548+niknal357@users.noreply.github.com>
2024-07-16 08:31:48 +0200

    remove unnecessary colorizer comments

--- Scripts/MTMultitool/modes/Colorizer.lua
+++ Scripts/MTMultitool/modes/Colorizer.lua
@@ -141,20 +141,6 @@ function Colorizer.trigger(multitool, primaryState, secondaryState, forceBuild, 
     if not self.displayingGUI then
         HoveringUI.cleanUp(multitool)
         multitool.VolumeSelector.modes = nil
-        -- 40 colors
-        -- multitool.VolumeSelector.modes = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40 }
-        -- multitool.VolumeSelector.modesNice = {
-        --     "White", "Light Gray", "Dark Gray", "Black",
-        --     "Light Yellow", "Yellow", "Dim Yellow", "Dark Yellow",
-        --     "Light Lime", "Lime", "Dim Lime", "Dark Lime",
-        --     "Light Green", "Green", "Dim Green", "Dark Green",
-        --     "Light Cyan", "Cyan", "Dim Cyan", "Dark Cyan",
-        --     "Light Blue", "Blue", "Dim Blue", "Dark Blue",
-        --     "Light Purple", "Purple", "Dim Purple", "Dark Purple",
-        --     "Light Magenta", "Magenta", "Dim Magenta", "Dark Magenta",
-        --     "Light Red", "Red", "Dim Red", "Dark Red",
-        --     "Light Orange", "Orange", "Light Brown", "Dark Brown"
-        -- }
         multitool.VolumeSelector.actionWord = "Colorize"
         multitool.VolumeSelector.isBeta = false
         multitool.VolumeSelector.selectionMode = "inside"

d4535d6f4a683477150ebfe7a77316bba4977c81
niknal357 <46793548+niknal357@users.noreply.github.com>
2024-07-24 09:00:52 +0200

    connection preview is now always white

--- Scripts/MTMultitool/ConnectionManager.lua
+++ Scripts/MTMultitool/ConnectionManager.lua
@@ -79,13 +79,13 @@ function ConnectionManager.client_onUpdate(multitool)
                     goto continue
                 end
 
-                -- local color = sm.color.new(1, 1, 1, 1)
-                local color = sm.MTFastLogic.FastLogicBlockColors[FastLogicAllBlockManager.blockUuidToConnectionColorID[tostring(task.from.uuid)]+1]
-                if color == nil then
-                    color = sm.color.new(1, 1, 1, 1)
-                else
-                    color = sm.color.new(color.r*0.7+0.3, color.g*0.7+0.3, color.b*0.7+0.3, 1)
-                end
+                local color = sm.color.new(1, 1, 1, 1)
+                -- local color = sm.MTFastLogic.FastLogicBlockColors[FastLogicAllBlockManager.blockUuidToConnectionColorID[tostring(task.from.uuid)]+1]
+                -- if color == nil then
+                --     color = sm.color.new(1, 1, 1, 1)
+                -- else
+                --     color = sm.color.new(color.r*0.7+0.3, color.g*0.7+0.3, color.b*0.7+0.3, 1)
+                -- end
                 if self.mode == "disconnect" then
                     color = sm.color.new(1, 0, 0, 1)
                 end
